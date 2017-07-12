1cc92003bf593b195ca6243fb1af0b89023b2bdd
hawkroc <yinpengroc@gmail.com>
2017-07-12 18:17:33 +1200

    'final'

---
+++ server/SynchronizeData.js
@@ -0,0 +1,6 @@
+const address="0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae";
+const baseUrl="http://api.etherscan.io/api?module=account&action=txlist&address=";
+const start="";
+const end="";
+const key="&apikey=E9MYVKUN5TNUBH6P4E5IWEUHAXGZCXQSNV";
+//"&startblock=11063&endblock=4009538&sort=asc"
